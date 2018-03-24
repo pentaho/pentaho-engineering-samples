12a2827cf20d2ca559a0a683398342abe49295a1
COLAS Benoit <benoit_colas@msn.com>
2018-03-24 07:54:36 +0100

    Add Registration of Saml SavedRequest Service

--- Samples for Extending Pentaho/Reference Implementations/Security/SAML 2.0/pentaho-saml-ss2-proxies-extension/src/main/java/org/pentaho/platform/spring/security/saml/ss2/proxies/creators/ProxyCreatorActivator.java
+++ Samples for Extending Pentaho/Reference Implementations/Security/SAML 2.0/pentaho-saml-ss2-proxies-extension/src/main/java/org/pentaho/platform/spring/security/saml/ss2/proxies/creators/ProxyCreatorActivator.java
@@ -6,16 +6,21 @@ import org.osgi.framework.ServiceRegistration;
 import org.pentaho.platform.proxy.api.IProxyCreator;
 import org.pentaho.platform.spring.security.saml.ss2.proxies.creators.authentication.S2ExpiringUsernameAuthenticationProxyCreator;
 import org.pentaho.platform.spring.security.saml.ss2.proxies.creators.authentication.S2SamlAuthenticationProxyCreator;
+import org.pentaho.platform.spring.security.saml.ss2.proxies.creators.savedrequest.S2SamlSavedRequestProxyCreator;
 
 public class ProxyCreatorActivator implements BundleActivator {
 
   ServiceRegistration s2SamlAuthenticationProxyServiceRegistration;
   ServiceRegistration s2ExpiringUsernameAuthenticationServiceRegistration;
+  ServiceRegistration s2SamlSavedRequestServiceRegistration;
 
   @Override public void start( BundleContext bundleContext ) throws Exception {
 
     s2SamlAuthenticationProxyServiceRegistration =
         bundleContext.registerService( IProxyCreator.class, new S2SamlAuthenticationProxyCreator(), null );
+    
+    s2SamlSavedRequestServiceRegistration =
+            bundleContext.registerService( IProxyCreator.class, new S2SamlSavedRequestProxyCreator(), null );
 
     s2ExpiringUsernameAuthenticationServiceRegistration =
         bundleContext.registerService( IProxyCreator.class, new S2ExpiringUsernameAuthenticationProxyCreator(), null );
@@ -26,6 +31,10 @@ public class ProxyCreatorActivator implements BundleActivator {
     if( s2SamlAuthenticationProxyServiceRegistration != null ) {
       s2SamlAuthenticationProxyServiceRegistration.unregister();
     }
+    
+    if( s2SamlSavedRequestServiceRegistration != null ) {
+    	s2SamlSavedRequestServiceRegistration.unregister();
+      }
 
     if( s2ExpiringUsernameAuthenticationServiceRegistration != null ) {
       s2ExpiringUsernameAuthenticationServiceRegistration.unregister();
